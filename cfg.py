c7d886c95b357090e712da7b0157118ced0d96ec
L9029 <angeloca03@gmail.com>
2022-12-14 14:23:58 -0400

    Car Game V1.3 "Loadings the images of the cars and the locations of them"

--- cfg.py
+++ cfg.py
@@ -1,7 +1,7 @@
 import os
 
 car = os.path.join(os.getcwd(), "Images/car.png")
-other_car = os.path.join(os.getcwd(), "Images/otheCar.png")
+other_car = os.path.join(os.getcwd(), "Images/otherCar.png")
 
 WIDTH = 800
 HEIGHT = 800
--- main.py
+++ main.py
@@ -15,16 +15,29 @@ road_w = int(cfg.WIDTH/1.6)
 roadmark_w = int(cfg.WIDTH/80)
 
 # Draw Graphics
-pygame.draw.rect(screen, (50, 50, 50), (cfg.WIDTH/2 - road_w/2, 0, road_w, cfg.HEIGHT))
-pygame.draw.rect(screen, (255, 255, 255), (cfg.WIDTH/2 - roadmark_w/2, 0, roadmark_w, cfg.HEIGHT))
+pygame.draw.rect(screen, (50, 50, 50), (cfg.WIDTH/2 - road_w/2, 0, road_w, cfg.HEIGHT)) # Road rect
+pygame.draw.rect(screen, (255, 255, 255), (cfg.WIDTH/2 - roadmark_w/2, 0, roadmark_w, cfg.HEIGHT)) # Road middle mark rect
+# Road right and left mark rect
 pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 - road_w/2 + roadmark_w * 2, 0, roadmark_w, cfg.HEIGHT))
 pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 + road_w/2 - roadmark_w * 3, 0, roadmark_w, cfg.HEIGHT))
 
-pygame.display.update()
+# Car player image load
+car = pygame.image.load(cfg.car) # search the image
+car_loc = car.get_rect() # Car rect
+car_loc.center = cfg.WIDTH / 2 + road_w / 4, cfg.HEIGHT * 0.8 # Car location
+
+# Car enemy image load
+car2 = pygame.image.load(cfg.other_car) # search the image
+car2_loc = car2.get_rect() # Car2 rect
+car2_loc.center = cfg.WIDTH / 2 - road_w / 4, cfg.HEIGHT * 0.2 # Car2 location
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+    
+    screen.blit(car, car_loc) # Car Graphic
+    screen.blit(car2, car2_loc) # Car2 Graphic
+    pygame.display.update()
 
 pygame.quit()
