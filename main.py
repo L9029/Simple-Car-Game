1c853f58e59ac582a5b3b2e11adc09472f8a3697
L9029 <angeloca03@gmail.com>
2022-12-14 16:56:28 -0400

    Car Game V1.4 "Animating the moving of the player and the enemy"

--- main.py
+++ main.py
@@ -14,13 +14,6 @@ pygame.display.set_caption("Cars") # This set title
 road_w = int(cfg.WIDTH/1.6)
 roadmark_w = int(cfg.WIDTH/80)
 
-# Draw Graphics
-pygame.draw.rect(screen, (50, 50, 50), (cfg.WIDTH/2 - road_w/2, 0, road_w, cfg.HEIGHT)) # Road rect
-pygame.draw.rect(screen, (255, 255, 255), (cfg.WIDTH/2 - roadmark_w/2, 0, roadmark_w, cfg.HEIGHT)) # Road middle mark rect
-# Road right and left mark rect
-pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 - road_w/2 + roadmark_w * 2, 0, roadmark_w, cfg.HEIGHT))
-pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 + road_w/2 - roadmark_w * 3, 0, roadmark_w, cfg.HEIGHT))
-
 # Car player image load
 car = pygame.image.load(cfg.car) # search the image
 car_loc = car.get_rect() # Car rect
@@ -32,9 +25,32 @@ car2_loc = car2.get_rect() # Car2 rect
 car2_loc.center = cfg.WIDTH / 2 - road_w / 4, cfg.HEIGHT * 0.2 # Car2 location
 
 while running:
+    #Animate enemy vehicle
+    car2_loc[1] += 1
+    
+    if car2_loc[1] > cfg.HEIGHT:
+        car2_loc[1] = -200
+    
+    # Game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+        
+        if event.type == KEYDOWN:
+            # Move the car to the left
+            if event.key in [K_a, K_LEFT]:
+                car_loc = car_loc.move([-int(road_w / 2), 0])
+            # Move the car to the right    
+            if event.key in list((K_d, K_RIGHT)):
+                car_loc = car_loc.move([int(road_w / 2), 0])
+    
+    # Draw Graphics
+    pygame.draw.rect(screen, (50, 50, 50), (cfg.WIDTH/2 - road_w/2, 0, road_w, cfg.HEIGHT)) # Road rect
+    pygame.draw.rect(screen, (255, 255, 255), (cfg.WIDTH/2 - roadmark_w/2, 0, roadmark_w, cfg.HEIGHT)) # Road middle mark rect
+    # Road right and left mark rect
+    pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 - road_w/2 + roadmark_w * 2, 0, roadmark_w, cfg.HEIGHT))
+    pygame.draw.rect(screen, (220, 240, 240), (cfg.WIDTH/2 + road_w/2 - roadmark_w * 3, 0, roadmark_w, cfg.HEIGHT))
+
     
     screen.blit(car, car_loc) # Car Graphic
     screen.blit(car2, car2_loc) # Car2 Graphic
